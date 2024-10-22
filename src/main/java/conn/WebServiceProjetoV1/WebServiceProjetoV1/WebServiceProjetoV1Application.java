8c981ae92dac2485cf0d3317d73dc3532a02dbfb
Carlosgabriel62 <124841314+Carlosgabriel62@users.noreply.github.com>
2024-10-22 12:15:28 +0000

    Adicionando arquivos exportados

---
+++ src/main/java/conn/WebServiceProjetoV1/WebServiceProjetoV1/WebServiceProjetoV1Application.java
@@ -0,0 +1,13 @@
+package conn.WebServiceProjetoV1.WebServiceProjetoV1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceProjetoV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceProjetoV1Application.class, args);
+	}
+
+}
