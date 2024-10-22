63807e5270c34ec3f3b153af91bf9ec212ff33d6
Carlosgabriel62 <124841314+Carlosgabriel62@users.noreply.github.com>
2024-10-22 13:11:42 +0000

    usarios

---
+++ src/main/java/conn/WebServiceProjetoV1/WebServiceProjetoV1/Usuario.java
@@ -0,0 +1,19 @@
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Getter
+@Settter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Usuario{
+
+    private String nome;
+    private int id;
+    private String email;
+
+
+    
+}
